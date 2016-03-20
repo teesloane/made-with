27f81cf4ec43c7f8a366d45bcd3cac9aa51d158b
Aaron Gray <aaron@hellometa.com>
2016-03-20 13:30:39 -0600

    Refactor getItem to remove randomization in preperation for tests. Export formattedPhrase for production use.

--- index.js
+++ index.js
@@ -10,23 +10,29 @@ var util = {
     return Math.floor(Math.random() * arr.length);
   },
 
-  getRandomItem: function (arr, ignoreItem) {
-    var arrCopy = arr.slice();
+  getItem: function (arr, index, ignoreItem) {
+    var arrCopy = arr.slice(),
+        selection = arrCopy[index];
 
-    if (ignoreItem) arrCopy.splice(arrCopy.indexOf(ignoreItem), 1)
-
-    return arrCopy[this.getRandomIndex(arrCopy)];
+    if (selection === ignoreItem) {
+      arrCopy.splice(arrCopy.indexOf(ignoreItem), 1);
+      return arrCopy[index];
+    }
+    return selection;
   }
 }
 
 var phrase = {
   choose: function (ignorePhrase) {
-    return util.getRandomItem(phrases, ignorePhrase);
+    var index = util.getRandomIndex(phrases);
+    return util.getItem(phrases, index);
   },
 
   build: function (first, second) {
-    var firstStyle = util.getRandomItem(chalkStyles);
-    var secondStyle = util.getRandomItem(chalkStyles, firstStyle);
+    var firstIndex = util.getRandomIndex(chalkStyles),
+        secondIndex = util.getRandomIndex(chalkStyles),
+        firstStyle = util.getItem(chalkStyles, firstIndex),
+        secondStyle = util.getItem(chalkStyles, secondIndex, firstStyle);
 
     return 'Made with ' + chalk[firstStyle](first) + ' and ' + chalk[secondStyle](second) + '.';
   },
@@ -42,3 +48,5 @@ var finalPhrase = phrase.build(phraseOne, phraseTwo);
 var formattedPhrase = phrase.format(finalPhrase);
 
 console.log(formattedPhrase);
+
+module.exports = formattedPhrase
